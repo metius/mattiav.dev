53f6e274ac8d55d82be4a083ee89afc2186a5a55
mattia <mattiav.dev@gmail.com>
2021-12-19 08:25:56 +0200

    · modified Menu component: added state and useEffectLayout to resolve build issue (SSR - window object not detected)

--- src/components/Header/Menu/Menu.component.jsx
+++ src/components/Header/Menu/Menu.component.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {useState, useLayoutEffect} from "react";
 import * as styles from './Menu.module.scss';
 import MobileMenu from "./MobileMenu.component";
 import DesktopMenu from "./DesktopMenu.component";
 
+const isBrowser = typeof window !== undefined;
+
 const Menu = () => {
 
+  const mobileBreakpoint = 1200;
+  const [screenWidth, setScreenWidth] = useState(typeof window !== "undefined" ? window.innerWidth : mobileBreakpoint);
+
+  useLayoutEffect(() => {
+    const handleWindowResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener("resize", handleWindowResize);
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
+
   const menuItems = [
-    // {
-    //   name: 'Home',
-    //   link: '/',
-    //   icon: 'home'
-    // },
     {
       name: 'About',
       link: '/#about',
@@ -31,7 +37,7 @@ const Menu = () => {
   return(
     <div id={styles.menu}>
     {
-      window.innerWidth > 1200 ? <DesktopMenu menuItems={menuItems} /> : <MobileMenu menuItems={menuItems} />
+      screenWidth > 1200 ? <DesktopMenu menuItems={menuItems} /> : <MobileMenu menuItems={menuItems} />
     }      
     </div>
   )
